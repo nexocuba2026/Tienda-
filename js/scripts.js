58ae36d816951f4cf5deaadf0e54a560d84907c9
nexocuba <nexocuba2026@gmail.com>
2026-02-24 22:46:57 -0500

    Crear js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// Funcionalidad slider en todas las tarjetas
+document.querySelectorAll('.tarjeta').forEach(tarjeta => {
+    const slides = tarjeta.querySelectorAll('img');
+    let index = 0;
+
+    const prev = tarjeta.querySelector('.prev');
+    const next = tarjeta.querySelector('.next');
+
+    function showSlide(i) {
+        slides.forEach(slide => slide.classList.add('hidden'));
+        slides[i].classList.remove('hidden');
+    }
+
+    prev.addEventListener('click', () => {
+        index = (index === 0) ? slides.length - 1 : index - 1;
+        showSlide(index);
+    });
+
+    next.addEventListener('click', () => {
+        index = (index === slides.length - 1) ? 0 : index + 1;
+        showSlide(index);
+    });
+
+    showSlide(index);
+});
